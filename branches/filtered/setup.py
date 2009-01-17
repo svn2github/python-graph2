5fc8bd7714b853f92de57a42f57cc77bfcab971c
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2009-01-17 18:40:41 +0000

    new branch for studying filtered search
    git-svn-id: http://python-graph.googlecode.com/svn@316 70df0079-b534-0410-988b-a5721c0f2d16

---
+++ branches/filtered/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import logging
+
+try:
+    from setuptools import setup, find_packages
+except ImportError, ie:
+    import ez_setup
+    ez_setup.use_setuptools()
+
+# Startup
+appname = "python-graph"
+appversion = "1.4.0"
+
+# Extra files
+if (os.name == 'posix'):    # Files to be installed/packaged on Unix-like systems
+    datadir = 'share/doc/'+appname+appversion
+    datafiles = ['README', 'COPYING', 'Changelog']
+    docsdir = datadir + '/docs'
+    docsfiles = []
+    try:
+        dirlisting = os.listdir('docs/')
+    except:
+        print "Documentation isn't present and will not be installed/packaged."
+        dirlisting = []
+    for each in dirlisting:
+        docsfiles.append('docs/'+each)
+else:    # Other systems
+    datadir = ''
+    datafiles = []
+    docsdir = ''
+    docsfiles = []
+
+setup(
+        name = appname,
+        version = appversion,
+        packages = ['graph', 'graph.algorithms', 'graph.algorithms.heuristics', 'graph.classes'],
+        data_files = [(docsdir,docsfiles),
+                       (datadir,datafiles)],
+        author = "Pedro Matiello",
+        author_email = "pmatiello@gmail.com",
+        description = "A library for working with graphs in Python",
+        license = "MIT",
+        keywords = "python graph algorithms library",
+        url = "http://code.google.com/p/python-graph/",
+        classifiers = ["License :: OSI Approved :: MIT License","Topic :: Software Development :: Libraries :: Python Modules"],
+        long_description = "python-graph is a library for working with graphs in Python. This software provides a suitable data structure for representing graphs and a whole set of important algorithms.",
+        test_suite = "graph.tests",
+)
