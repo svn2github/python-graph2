7123f06899ea3480a53841076350faef9c60c23b
salimfadhley <salimfadhley@70df0079-b534-0410-988b-a5721c0f2d16>
2009-10-01 23:04:46 +0000

    A new branch for python3 refactoring
    git-svn-id: http://python-graph.googlecode.com/svn@541 70df0079-b534-0410-988b-a5721c0f2d16

---
+++ branches/python3_refactor/core/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import logging
+
+try:
+    from setuptools import setup, find_packages
+except ImportError, ie:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+# Startup
+appname = "python-graph-core"
+appversion = open("../version.txt").read()
+
+# Extra files
+if (os.name == 'posix'):    # Files to be installed/packaged on Unix-like systems
+    datadir = 'share/doc/'+appname+'-'+appversion
+    datafiles = ['../README', '../COPYING', '../Changelog']
+    docsdir = datadir + '/docs'
+    docsfiles = []
+    try:
+        # Uncomment the line bellow if you want bdist_rpm to include the docs
+        os.system('make docs')
+        dirlisting = os.listdir('docs/')
+    except:
+        print "Documentation isn't present and will not be installed/packaged."
+        dirlisting = []
+    for each in dirlisting:
+        docsfiles.append('docs/'+each)
+else:    # Other systems
+    datadir = ''
+    datafiles = []
+    docsdir = ''
+    docsfiles = []
+
+setup(
+        name = appname,
+        version = appversion,
+        data_files = [(docsdir,docsfiles),
+                       (datadir,datafiles)],
+        author = "Pedro Matiello",
+        namespace_packages = ["pygraph"],
+        packages = ["pygraph"] + [ os.path.join("pygraph", a) for a in find_packages("pygraph") ],
+        author_email = "pmatiello@gmail.com",
+        description = "A library for working with graphs in Python",
+        license = "MIT",
+        keywords = "python graphs hypergraphs networks library algorithms",
+        url = "http://code.google.com/p/python-graph/",
+        classifiers = ["License :: OSI Approved :: MIT License","Topic :: Software Development :: Libraries :: Python Modules"],
+        long_description = "python-graph is a library for working with graphs in Python. This software provides a suitable data structure for representing graphs and a whole set of important algorithms.",
+)
