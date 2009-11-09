4b24679717c2a6bc15925438bfd19886331707b1
christian.muise <christian.muise@70df0079-b534-0410-988b-a5721c0f2d16>
2009-11-09 16:05:46 +0000

    Cleaning out the old branch since its been merged.
    git-svn-id: http://python-graph.googlecode.com/svn@602 70df0079-b534-0410-988b-a5721c0f2d16

--- branches/generic_edge/core/pygraph/mixins/basegraph.py
+++
@@ -1,6 +0,0 @@
-class basegraph( object ):
-    """
-    An abstract class intended as a common ancestor to all graph classes. This allows the user to test isinstance(X, basegraph) to determine if 
-    the object is one of any of the python-graph main classes.
-    """
-    
